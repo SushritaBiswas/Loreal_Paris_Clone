e2d179e6fce62de2a8508c8f52f6aa5a73f71652
SushritaBiswas <sushritabiswas19@gmail.com>
2026-01-09 20:04:52 +0530

    HEADER FIXED. HEADER BACKGROUND COMPONENT CREATED

--- src/global/Header/Header.tsx
+++ src/global/Header/Header.tsx
@@ -3,9 +3,9 @@ import Navbar from "./components/Navbar";
 
 export default function Header() {
   return (
-    <section className="max-w-500 w-full max-h-36 bg-amber-200 mx-auto py-5 hover:bg-white duration-200">
+    <section className="max-w-500 group w-full max-h-36 bg-amber-200 mx-auto py-5 hover:bg-white duration-200">
       {/* Image */}
-      <div className="invert hover:invert-0 duration-200 relative max-w-60 w-full aspect-4/1 cursor-pointer mx-auto">
+      <div className="invert group-hover:invert-0 duration-200 relative max-w-60 w-full aspect-4/1 cursor-pointer mx-auto">
         <Image
           src="/loreal-paris-black-logo.svg"
           alt="Loreal Paris Logo"
--- src/global/Header/components/Navbar.tsx
+++ src/global/Header/components/Navbar.tsx
@@ -2,14 +2,13 @@ import { NavbarData } from "@/data/Navbar.data";
 
 export default function Navbar() {
   return (
-    <section className="max-w-500 w-full flex justify-center py-4 gap-10">
+    <section className="max-w-500 w-full flex justify-center py-4 gap-12">
       {NavbarData.map((items, index) => (
         <div
           key={index}
-          className="uppercase text-[10px] text-white"
+          className="uppercase text-[10px] cursor-pointer group-hover:invert text-white hover:text-tertiary font-normal p-2"
         >
           {items.menu}
-          <div></div>
         </div>
       ))}
     </section>
